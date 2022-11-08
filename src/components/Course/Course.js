a6df92e97d9358c754816cdb6723bb5bd1c5be79
@thong3101 <19110046@student.hcmute.edu.vn>
2022-11-08 16:57:07 +0700

    dashboard

---
+++ src/components/Course/Course.js
@@ -0,0 +1,18 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import img from "assets/img/ProjectImage3.png";
+
+export function Course() {
+  return (
+    <>
+      <Box sx={{ width: "256px", height: "170px", background:"#ffff" }}>
+        <img alt="" style={{ width: "100%", height: "84px" }} src={img} />
+        <Box sx={{marginTop:"10px"}}>
+          <Text>Tiêu đề</Text>
+          <Text>Mô tả ngắn gọn</Text>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,10 +1,44 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Stack, Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { SearchBar } from "components/Navbars/SearchBar/SearchBar";
+import { Course } from "components/Course/Course";
 
 function Dashboard() {
   return (
     <>
-      <Button>hello</Button>
+      <Stack direction={["column"]} spacing={5} sx={{ marginTop: "100px", marginLeft: "100px" }}>
+        <Box>
+          <Stack direction={["row"]} spacing={100}>
+            <SearchBar />
+            <Button
+              variant="contained"
+              sx={{
+                width: "120px",
+                background: "#9095a1",
+                color: "#ffffff",
+                fontFamily: "Plus Jakarta Display"
+              }}
+            >
+              Sắp xếp
+            </Button>
+          </Stack>
+        </Box>
+
+        <Box
+          sx={{
+            margin: "30px 100px",
+            width: "200px",
+            height: "40px",
+            textAlign: "center",
+            color: "#ffffff"
+          }}
+        >
+          <Text sx={{ margin: "8px" }}>Khóa học mới nhất</Text>
+        </Box>
+        <Box sx={{ margin: "20px 80px", width: "600px" }}>
+          <Course />
+        </Box>
+      </Stack>
     </>
   );
 }
